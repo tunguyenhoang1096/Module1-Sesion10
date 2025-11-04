282311a94f247fe6b4af5a18e4b5b68ca4eacb83
tunguyenhoang1096 <tunguyenhoang1096@gmail.com>
2025-11-04 23:00:55 +0900

    Add files via upload

---
+++ ex1/main.js
@@ -0,0 +1,4 @@
+alert("Hello world"); // hiển thị 1 thông báo khi mở hoặc tải lại trang
+alert(`Hello world`); //có thể dùng dấu huyền( dưới phím esc) để hiển thị
+console.log("Hello world"); //Hiển thị trong hộp thoại console.log
+document.write("Hello world"); //hiện thị trực tiếp lên trang html
