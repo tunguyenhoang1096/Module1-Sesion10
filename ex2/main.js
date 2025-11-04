b006db749180efa72e0aabee230a7602be54e2cb
tunguyenhoang1096 <tunguyenhoang1096@gmail.com>
2025-11-04 23:01:10 +0900

    Add files via upload

---
+++ ex2/main.js
@@ -0,0 +1,12 @@
+// Khởi tạo biến i kiểu số nguyên giá trị bằng 10
+let i = 10;
+// khởi tạo biến f kiểu số thực giá trị bằng 20.5
+let f = 20.5;
+// khởi tạo biến b kiểu logic(Boolane)
+let b = true;
+// khởi tạo biến s kiểu chuỗi(string) có giá trị là "Hà Nội"
+let s = "Hà Nội";
+document.write("Số nguyên i bằng: " + i, "<br />");
+document.write("Số thực f bằng: " + f, "<br />");
+document.write("Kiểu boolane có giá trị là: " + b, "<br />");
+document.write("Kiểu chuỗi string: " + s);
