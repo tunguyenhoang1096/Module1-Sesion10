38ec1910769d4bef5cd0117fa079296d53f72602
tunguyenhoang1096 <tunguyenhoang1096@gmail.com>
2025-11-04 23:01:42 +0900

    Add files via upload

---
+++ ex4/main.js
@@ -0,0 +1,8 @@
+// khởi tạo biến chứa giá trị số thứ nhất khi ng dùng nhập
+let fisrtNumber = Number(prompt("Nhập số thứ nhất:"));
+// khởi tạo biến chứa giá trị số thứ hai khi ng dùng nhập
+let secondNumber = parseFloat(prompt("Nhập số thứ hai:"));
+// khởi tạo biến tổng của 2 số vừa nhập
+let sum = fisrtNumber + secondNumber;
+// hiển thị ra màn hình tổng của 2 số vừa nhập
+alert("Tổng của " + fisrtNumber + " và " + secondNumber + " là: " + sum);
